5fdc0aaf0b8af592682772d4b19073d2418c7482
jj <Aziz@MacBook-Aziz.local>
2023-09-20 15:04:51 +0300

    vvv

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-
+const aoo =1;
 app.listen(port, function () {
   console.log(`Server app listening on port ${port}`);
 });
