c68156d5f509e2068480598e3c35d403b437b699
isIbra <105174960+isIbra@users.noreply.github.com>
2023-09-22 03:41:39 +0300

    express server running and receving data from esp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-
 const app = express();
 const port = 3000;
 
-app.listen(port, function () {
-  console.log(`Server app listening on port ${port}`);
+app.use(express.json());
+
+// Define routes for communication with the ESP32
+app.post('/esp', (req, res) => {
+  console.log('a vistor');
+  const dataFromESP32 = req.body;
+
+  console.log('Received data from ESP32:', req.body); //receing data from ESP32
+  res.send('Data received successfully');
+});
+
+app.get('/esp', (req, res) => {
+  //checking for esp32
+  console.log('a vistor');
+});
+
+app.listen(port, '192.168.100.13', () => {
+  console.log(`Express server listening at http://192.168.100.13:${port}`);
 });
